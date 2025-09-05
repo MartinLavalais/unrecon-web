65524e2f168ec4c2195908ce8da10f70ece96039
james-bnn <james@benone.ch>
2025-09-05 16:55:42 +0200

    Ajout de la page a propos

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,140 @@
 export default function AboutPage() 
 {
     return(
-        <h1>test</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen p-6 space-y-6 bg-base-100">
+            <div className="hero bg-base-200 min-h-screen">
+              <div className="hero-content flex-col lg:flex-row">
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                  className="max-w-sm rounded-lg shadow-2xl"
+                />
+                <div>
+                  <h1 className="text-5xl font-bold">Qui suis-je ?</h1>
+                  <p className="py-6">
+                    Je m'appel James Benone, 19 ans, de nationalité Suisse et actuellement en étude.
+                    Je suis un développeur passionné, j'adore me lancé des défis et apprendre de nouvelle chose.
+                    J'aime la musique, sortir avec mes amis et repousser mes limites.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+              <li>
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="text-success h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="timeline-start mb-10 md:text-end">
+                  <time className="font-mono italic">2021</time>
+                  <div className="text-lg font-black">Début d'apprentissage</div>
+                  J'ai commencé un apprentissage au CFPT Informatique dans le domaine du développement d'application.
+                </div>
+                <hr className="bg-success" />
+              </li>
+              <li>
+                <hr className="bg-success" />
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="text-success h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="timeline-end md:mb-10">
+                  <time className="font-mono italic">2023-2025</time>
+                  <div className="text-lg font-black">Président des délégués</div>
+                  Durant 2 ans, j'ai été le président des délégués. J'étais chargé d'organsier des réunions avec les délégués,
+                  De transmettre les information entre la direction et les élèves. Je devais être à l'écoute des élèvesœt trouver un
+                  terrain d'entente entre la direction et les élèves sur des sujets divers et variés.
+                </div>
+                <hr className="bg-success" />
+              </li>
+              <li>
+                <hr className="bg-success" />
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="text-success h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="timeline-end md:mb-10">
+                  <time className="font-mono italic">2025</time>
+                  <div className="text-lg font-black">Obtention de mon CFC</div>
+                  J'ai obtenu mon CFC développement d'application avec mention bien.
+                </div>
+                <hr className="bg-success" />
+              </li>
+              <li>
+                <hr className="bg-success" />
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="text-success h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="timeline-start mb-10 md:text-end">
+                  <time className="font-mono italic">2025</time>
+                  <div className="text-lg font-black">Formation Informaticien E.S.</div>
+                  Début de ma formation en Informaticien E.S. au CFPT Informatique.
+                </div>
+                <hr className="bg-warning" />
+              </li>
+              <li>
+                <hr className="bg-warning" />
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="timeline-end mb-10 md:text-start">
+                  <time className="font-mono italic">2027?</time>
+                  <div className="text-lg font-black">Obtention du diplôme Informaticien E.S.</div>
+                  Mon objectif actuel et d'arrivé jusqu'à là et trouver une entreprise qui pourrait m'accepter pour travailler dans le domaine informatique.
+                </div>
+              </li>
+            </ul>
+        </div>
     );
 }
