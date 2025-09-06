b681ce4ed23602f5464651ea9687b46af47f2159
james-bnn <james@benone.ch>
2025-09-06 13:23:39 +0200

    Creation de la page de prise de contact

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage()
+{
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen p-6 space-y-6 bg-base-100">
+            <h1>En construction</h1>
+        </div>
+    );
+}
